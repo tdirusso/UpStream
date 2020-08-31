b72482928d3000559eb38d5b7d50b2b77370565a
Timothy DiRusso <timgdirusso@gmail.com>
2020-08-30 22:15:21 -0700

    Creating upstream controller class

--- upstream/electron/main.js
+++ upstream/electron/main.js
@@ -1,40 +1,29 @@
 const electron = require('electron');
-const { app, BrowserWindow, screen, ipcMain } = electron;
+const { app, screen, ipcMain } = electron;
 const path = require('path');
+const UpStream = require('./UpStream');
 
 const isDev = true;
-
-const paths = {
-    createUser: '#/createUser'
-};
-
-let window;
 
 app.on('ready', () => {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-    window = new BrowserWindow({
+    UpStream.start({
+        startURL: isDev ? 'http://localhost:3000/' : `file://${path.join(__dirname, '../build/index.html')}`,
         width: width - 500,
         height: height - 200,
-        webPreferences: {
+        preferences: {
             nodeIntegration: true
         }
-    });
-
-    const startURL = isDev ? 'http://localhost:3000/' : `file://${path.join(__dirname, '../build/index.html')}`;
-
-    window.loadURL(startURL);
-
-    window.webContents.openDevTools();
-
-    window.on('closed', () => {
-        app.quit();
     });
 });
 
 ipcMain.on('intro:done', () => {
     setTimeout(() => {
-        window.webContents.executeJavaScript(`location.assign('${paths.createUser}')`);
-        window.focus();
+        if (UpStream.users.length > 0) {
+            //pick user
+        } else {
+            UpStream.showView(UpStream.views.createUser);
+        }
     }, 3500);
 });
--- upstream/src/index.js
+++ upstream/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Intro from './components/Intro/Intro';
+import Intro from './components/Intro/Intro';
 import CreateUser from './components/CreateUser/CreateUser';
 import { HashRouter, Route } from 'react-router-dom';
 
@@ -9,8 +9,8 @@ ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter>
 			<div className="main-container">
-				{/* <Route path="/createUser" exact component={CreateUser} /> */}
-				<Route path="/" exact component={CreateUser} />
+				<Route path="/createUser" exact component={CreateUser} />
+				<Route path="/" exact component={Intro} />
 			</div>
 		</HashRouter>
 	</React.StrictMode>,
